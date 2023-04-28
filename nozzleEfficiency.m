468a05a7ae0f9ac9642fdcefa157b83cac3374ce
Waterdrinker88 <48976062+Waterdrinker88@users.noreply.github.com>
2023-04-28 17:13:24 -0500

    nozzle efficency goooood

---
+++ nozzleEfficiency.m
@@ -0,0 +1,22 @@
+%Nicholas Archon, nga425
+%COE 347 - Introduction to Computational Fluid Dynamics
+%OF4 - Determines Nozzle Efficiency
+%Formulas taken from Rocket Propulsion Elements, 9th, Sutton & Biblarz.
+clear;clc;close all;
+
+gamma = 1.4; 
+T01 = 1; 
+ve = 1.65;
+PeP01 = 0.16; 
+cp = 3.5; 
+n_n = nozzleEff(ve,cp,T01,PeP01,gamma)
+
+function n_n = nozzleEff(ve,cp,T01,PeP01,g)
+    term1 = ve.^2; 
+    term2 = 2.*cp.*T01;
+    term3 = 1 - PeP01.^((g-1)./g); 
+
+    n_n = term1./(term2.*term3); 
+
+
+end 
